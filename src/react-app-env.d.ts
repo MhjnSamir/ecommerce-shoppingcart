5edec0a876c31705f10f608d964ae339e9800d30
rabin <shrestharabin68@gmail.com>
2020-12-01 16:08:21 +0545

    fix: start bugs

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -7,3 +7,18 @@
 interface Window { 
     jQuery: any;
 }
+
+declare module 'react-pivottable/PivotTableUI' {
+    const PivotTableUI: any;
+    export default PivotTableUI;
+}
+
+declare module 'react-pivottable/TableRenderers' {
+    const TableRenderers: any;
+    export default TableRenderers;
+}
+
+declare module 'react-pivottable/PlotlyRenderers' {
+    const PlotlyRenderers: any;
+    export default PlotlyRenderers;
+}
