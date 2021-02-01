0a8983a10f60bba5078f96fccc22e4d0e1061484
rabin <shrestharabin68@gmail.com>
2021-02-01 17:08:07 +0545

    fix: design assets

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import thunk, { ThunkAction } from 'redux-thunk';
 
 import rootReducer, { RootState } from "./root-reducer";
 
-const composeEnhancer = (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose) || compose;
 const middleware = applyMiddleware(thunk);
 
 const store = createStore(rootReducer, composeEnhancer(middleware));
